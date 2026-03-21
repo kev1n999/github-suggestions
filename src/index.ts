d37c230c01d2b81aeb072691123798d37df50050
kev1n999 <sopratesta999@gmail.com>
2026-03-21 16:54:42 -0300

    first

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
 import { env } from "./config/env";
 import express from "express";
 import { router } from "./routes";
-import path from "path";
 
-const port = Number(env.SERVER_PORT!);
 const app = express();
 
 app.use(express.json());
 app.use(router);
-app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(port, () => {
-  console.log(`Server listening on: http://localhost:${port}`)
-});
+export default app;
